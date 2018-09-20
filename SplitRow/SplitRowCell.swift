0d0a8ed3dd1c66340c50ea089546c69a7a11f8d2
Bret Pudenz <bpudenz@gmail.com>
2018-09-20 15:50:45 -0500

    no separator

--- SplitRow/SplitRowCell.swift
+++ SplitRow/SplitRowCell.swift
@@ -27,7 +27,7 @@ open class SplitRowCell<L: RowType, R: RowType>: Cell<SplitRowValue<L.Cell.Value
 		
 		self.tableViewRight = SplitRowCellTableView()
 		tableViewRight.separatorStyle = .none
-		tableViewRight.leftSeparatorStyle = .singleLine
+		tableViewRight.leftSeparatorStyle = .none
 		tableViewRight.translatesAutoresizingMaskIntoConstraints = false
 		
 		contentView.addSubview(tableViewLeft)
